ce20c4d2a8a08bb33ceba4b72d4d606d05566c67
Alex <alexchvt@gmail.com>
2018-07-13 17:14:30 +0200

    Created components and router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CoachesComponent } from './coaches/coaches.component';
+import { CoachComponent } from './coach/coach.component';
+import { TraineeComponent } from './trainee/trainee.component';
+import { AgendaComponent } from './agenda/agenda.component';
+import { AppRoutingModule } from './/app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoachesComponent,
+    CoachComponent,
+    TraineeComponent,
+    AgendaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
