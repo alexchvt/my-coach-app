a589d13fc0b0a1465eef3e16b2519b200346650f
Alex Chavet <alexchvt@gmail.com>
2018-08-08 17:06:32 +0200

    Add nav header and scss config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { CoachComponent } from './coach/coach.component'
+import { CoachesComponent } from './coaches/coaches.component';
+import { CoachComponent } from './coach/coach.component';
+import { AgendaComponent } from './agenda/agenda.component';
 
 const routes : Routes = [
-  {path: 'coach', component: CoachComponent}
+  {path: 'coaches', component: CoachesComponent, data: { title: 'Coaches'}},
+  {path: 'agenda', component: AgendaComponent, data: { title: 'Agenda'}},
+  {path: 'coach', component: CoachComponent, data: { title: 'Coach Profile'}},
+
 
 ]
 
--- src/app/coach/coach.component.ts
+++ src/app/coach/coach.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-coach',
   templateUrl: './coach.component.html',
-  styleUrls: ['./coach.component.css']
+  styleUrls: ['./coach.component.scss']
 })
 export class CoachComponent implements OnInit {
 
