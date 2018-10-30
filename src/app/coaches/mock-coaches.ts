930a9711d6b971a2e6bdd94c829274176cf850b3
Alex <alexchvt@gmail.com>
2018-10-30 16:33:57 +0100

    Fix coaches row and improve menu

--- src/app/coaches/mock-coaches.ts
+++ src/app/coaches/mock-coaches.ts
@@ -14,13 +14,45 @@ export const mockCoaches: Coach[] = [{
     age: 32,
     activities: ["crossfit", "running"],
     city: "Paris",
-    profilePicture: "../assets/img/32.jpg"
+    profilePicture: "../assets/img/34.jpg"
 },
 {
     name: "Tracy",
     lastname: "McStretching",
     age: 26,
-    activities: ["Yoga","Stretching"],
+    activities: ["Aerobic","Stretching"],
+    city: "Paris",
+    profilePicture: "../assets/img/33.jpg"
+},
+{
+    name: "Stacy",
+    lastname: "VonYoga",
+    age: 24,
+    activities: ["Yoga","Gymnatic"],
+    city: "Paris",
+    profilePicture: "../assets/img/35.jpg"
+},
+{
+    name: "Tony",
+    lastname: "Danza",
+    age: 32,
+    activities: ["crossfit", "running"],
+    city: "Paris",
+    profilePicture: "../assets/img/34.jpg"
+},
+{
+    name: "Tracy",
+    lastname: "McStretching",
+    age: 26,
+    activities: ["Aerobic","Stretching"],
+    city: "Paris",
+    profilePicture: "../assets/img/33.jpg"
+},
+{
+    name: "Stacy",
+    lastname: "VonYoga",
+    age: 24,
+    activities: ["Yoga","Gymnatic"],
     city: "Paris",
     profilePicture: "../assets/img/35.jpg"
 }]
